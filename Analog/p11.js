0c50608a55b435b650aa944618eb835e81504d72
SHAIK MUHAMMED ABDUR REHMAN <amrehman3@gmail.com>
2024-06-26 12:53:50 +0530

    Project added

---
+++ Analog/p11.js
@@ -0,0 +1,22 @@
+let minHand = document.querySelector('.min-hand');
+let hourHand = document.querySelector('.hour-hand');
+let secHand = document.querySelector('.second-hand');
+
+function clock() {
+    let time = new Date();
+    let hrs = time.getHours();
+    let mins = time.getMinutes();
+    let sec = time.getSeconds();
+
+    hrs = (((hrs * 60) / 12) * 6) + 90;
+    hourHand.style.transform = `rotate(${hrs}deg)`;
+
+    mins = (mins * 6) + 90;
+    minHand.style.transform = `rotate(${mins}deg)`;
+
+    sec = (sec * 6) + 90;
+    secHand.style.transform = `rotate(${sec}deg)`;
+}
+
+setInterval(clock, 1000);
+clock();
---
+++ Front end/Test/test/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+class App extends React.Component{
+  render(){
+    return(
+      <div>
+      <center>
+      <h3> I'm from component</h3>
+      </center>
+      </div>
+    )
+
+    }
+  }
+
+  export default App;
